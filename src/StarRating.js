d41be498644e6236802252e54804bd0261b78976
Colin Huber <cbhuber@gmail.com>
2023-06-25 21:30:13 -0600

    feat: Prop types validators star rating.

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types"; // Use Typescript, not this
 
 const containerStyle = {
   display: "flex",
@@ -8,6 +9,16 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
+};
+
+// Use Typescript rather than propTypes below
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
 };
 
 export default function StarRating({
